3678301840b402e0c7d9215337732914338f2458
Ahmad Alghali <a.alghali@primeboard.org>
2023-05-01 04:36:11 +0100

    add delete entry endpoint

--- src/service/work-entry.service.ts
+++ src/service/work-entry.service.ts
@@ -5,4 +5,8 @@ async function submitWorkEntry(addWorkEntryRequestDTO: AddWorkEntryRequestDTO) {
   return api.post<AddWorkEntryResponseDTO>("/entries", addWorkEntryRequestDTO).then((res) => res.data);
 }
 
-export { submitWorkEntry };
+async function deleteEntry(id: number): Promise<boolean> {
+  return api.delete(`/entries/${id}`).then((res) => res.status == 200 || res.status == 204);
+}
+
+export { submitWorkEntry, deleteEntry };
